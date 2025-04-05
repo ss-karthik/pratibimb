1db4a265341dc602883b167ba0d962c99207ba22
ss-karthik <maniac.sskarthik@gmail.com>
2025-04-05 15:42:16 +0530

    Add DB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,25 @@
 import dotenv from "dotenv";
 import app from "./app.js"
+import connectDB from "./db/db.js"
 
 dotenv.config({
     path: "./env",
 });
 
-const port = process.env.PORT || 3000;
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server listening on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB Connection failed!! ", err);
+    });
 
 app.get("/", (req,res)=>{
     res.send("Pratibimb Backend!")
-})
-
-app.listen(port, ()=>{
-    console.log(`Server listening on port ${port}`);
 })
