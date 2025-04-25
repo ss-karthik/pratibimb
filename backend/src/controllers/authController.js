736384b3dd39ffa0314f61d449aaca566da2a67a
ss-karthik <maniac.sskarthik@gmail.com>
2025-04-25 11:32:47 +0530

    More cookie changes

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -43,7 +43,7 @@ export const signup = async (req,res)=>{
     try {
         const user = await User.create({email, password, deviceId, age, gender, smoker, alcoholic});
         const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000, sameSite: 'none',
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000, sameSite: 'Strict',
             partitioned: true
           });
         res.status(201).json({user: user._id});
